d20676940f9322473e74e8cd004a4be346eb1211
Peter <peterglen99@gmail.com>
2022-04-21 15:58:22 -0400

    autocheck

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyedpro",
-    version="2.1.3",
+    version="2.9.0",
     author="Peter Glen",
     author_email="peterglen99@gmail.com",
     description="High power editor in python.",
