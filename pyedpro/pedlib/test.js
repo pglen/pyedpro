c90e826e8b7ea6cda999b03e8858c22cf5cefd47
Peter <peterglen99@gmail.com>
2022-05-02 18:01:47 -0400

    autocheck

--- pyedpro/pedlib/test.js
+++ pyedpro/pedlib/test.js
@@ -1,10 +1,10 @@
-<script>
+"<script>"
 
 // Submit - and check
 
 function buttpress()
 {
-    //alert("Pressed");
+    alert("Pressed");
     //console.log("user name len " + document.getElementById("user").value.length)
     if(document.getElementById("user").value.length < 4)
         {
@@ -13,7 +13,13 @@ function buttpress()
         }
     var passx = document.getElementById("pass").value;
 
-    # Only go through hoops if there is a pass
+    if(passx.length == 0)
+        {
+        alert("Password cannot be empty.");
+        return;
+        }
+
+    // Only go through hoops if there is a pass
     if(passx.length > 1)
         {
         if(passx.length < 6)
@@ -29,6 +35,39 @@ function buttpress()
         }
     document.getElementById("frm").submit()
 }
+
+// Submit - and check
+
+function passpress()
+{
+    //alert("Pressed pass button");
+    console.log("Passpress");
+
+    var passx = document.getElementById("new_pass").value;
+
+    if(passx.length == 0)
+        {
+        alert("Password cannot be empty.");
+        return;
+        }
+
+    // Only go through hoops if there is a pass
+    if(passx.length > 1)
+        {
+        if(passx.length < 6)
+            {
+            alert("Pass must be 6 characters or more.");
+            return;
+            }
+        if(!hasUpperCase(passx) || !hasLowerCase(passx))
+            {
+            alert("Pass must have upper and lower case characters.");
+            return;
+            }
+        }
+    document.getElementById("passfrm").submit()
+}
+
 
 function hasUpperCase(parola){
  for(i = 0; i < parola.length; i++){
@@ -65,7 +104,7 @@ function hasLowerCase(parola){
     // #echo $sss2;
 -->
 
-</script>
+"</script>"
 
 
 
