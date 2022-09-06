41be8b3be1a87a5d7b47264871c4894dac314418
pglen <pglen@users.noreply.github.com>
2022-09-06 20:20:05 +0000

    deploy: 8f4d500f28d0e85b3559979189c09df5a2f20020

--- search/files_a.js
+++ search/files_a.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['readme_2emd_0',['README.md',['../AppDir_2pycommon_2README_8md.html',1,'(Global Namespace)'],['../AppDir_2README_8md.html',1,'(Global Namespace)'],['../AppImage_2pycommon_2README_8md.html',1,'(Global Namespace)'],['../AppImage_2README_8md.html',1,'(Global Namespace)'],['../pyedpro_2pycommon_2README_8md.html',1,'(Global Namespace)'],['../pyedpro_2README_8md.html',1,'(Global Namespace)']]]
+  ['readme_2emd_0',['README.md',['../AppDir_2pycommon_2README_8md.html',1,'(Global Namespace)'],['../AppDir_2README_8md.html',1,'(Global Namespace)'],['../AppImage_2pycommon_2README_8md.html',1,'(Global Namespace)'],['../AppImage_2README_8md.html',1,'(Global Namespace)'],['../pyedpro_2pycommon_2README_8md.html',1,'(Global Namespace)'],['../README_8md.html',1,'(Global Namespace)']]]
 ];
--- search/pages_0.js
+++ search/pages_0.js
@@ -3,5 +3,5 @@ var searchData=
   ['py_20common_0',['py Common',['../md_AppDir_pycommon_README.html',1,'(Global Namespace)'],['../md_AppImage_pycommon_README.html',1,'(Global Namespace)'],['../md_pyedpro_pycommon_README.html',1,'(Global Namespace)']]],
   ['pyedit_20pro_1',['Pyedit Pro',['../index.html',1,'']]],
   ['pyedpro_20key_20assignments_2',['PyEdPro Key Assignments',['../md_AppDir_KEYMAP.html',1,'(Global Namespace)'],['../md_AppImage_KEYMAP.html',1,'(Global Namespace)'],['../md_pyedpro_KEYMAP.html',1,'(Global Namespace)']]],
-  ['pyedpro_20readme_3',['PyEdPro README',['../md_AppDir_README.html',1,'(Global Namespace)'],['../md_AppImage_README.html',1,'(Global Namespace)'],['../md_pyedpro_README.html',1,'(Global Namespace)']]]
+  ['pyedpro_20readme_3',['PyEdPro README',['../md_AppDir_README.html',1,'(Global Namespace)'],['../md_AppImage_README.html',1,'(Global Namespace)'],['../md_README.html',1,'(Global Namespace)']]]
 ];
