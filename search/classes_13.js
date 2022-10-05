38625c364fb67d18e525fdea02b533f6a9eae0bb
pglen <pglen@users.noreply.github.com>
2022-10-05 14:43:10 +0000

    deploy: 1f5147cc65a244bd68f80091a36d8196411d9719

--- search/classes_13.js
+++ search/classes_13.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['widebutt_0',['WideButt',['../classAppDir_1_1pycommon_1_1pggui_1_1WideButt.html',1,'AppDir.pycommon.pggui.WideButt'],['../classAppImage_1_1pycommon_1_1pggui_1_1WideButt.html',1,'AppImage.pycommon.pggui.WideButt'],['../classpycommon_1_1pggui_1_1WideButt.html',1,'pycommon.pggui.WideButt'],['../classpyedpro_1_1pycommon_1_1pggui_1_1WideButt.html',1,'pyedpro.pycommon.pggui.WideButt']]]
+  ['widebutt_0',['WideButt',['../classAppDir_1_1pycommon_1_1pggui_1_1WideButt.html',1,'AppDir.pycommon.pggui.WideButt'],['../classAppImage_1_1pycommon_1_1pggui_1_1WideButt.html',1,'AppImage.pycommon.pggui.WideButt'],['../classpycommon_8old_1_1pggui_1_1WideButt.html',1,'pycommon.old.pggui.WideButt'],['../classpyedpro_1_1pycommon_1_1pggui_1_1WideButt.html',1,'pyedpro.pycommon.pggui.WideButt']]]
 ];
--- search/classes_8.js
+++ search/classes_8.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['imagebutton_0',['ImageButton',['../classwebview__1_1_1ImageButton.html',1,'webview_1']]],
-  ['invalidtype_1',['InvalidType',['../classAppDir_1_1pycommon_1_1pypacker_1_1InvalidType.html',1,'AppDir.pycommon.pypacker.InvalidType'],['../classAppImage_1_1pycommon_1_1pypacker_1_1InvalidType.html',1,'AppImage.pycommon.pypacker.InvalidType'],['../classpycommon_1_1pypacker_1_1InvalidType.html',1,'pycommon.pypacker.InvalidType'],['../classpyedpro_1_1pycommon_1_1pypacker_1_1InvalidType.html',1,'pyedpro.pycommon.pypacker.InvalidType']]],
-  ['itemhistory_2',['itemhistory',['../classAppDir_1_1pedlib_1_1pedwin_1_1itemhistory.html',1,'AppDir.pedlib.pedwin.itemhistory'],['../classAppImage_1_1pedlib_1_1pedwin_1_1itemhistory.html',1,'AppImage.pedlib.pedwin.itemhistory'],['../classpyedpro_1_1pedlib_1_1pedwin_1_1itemhistory.html',1,'pyedpro.pedlib.pedwin.itemhistory']]]
+  ['internal_5fsimplesel_1',['internal_SimpleSel',['../classpyedpro_1_1pycommon_1_1pgsimp_1_1internal__SimpleSel.html',1,'pyedpro::pycommon::pgsimp']]],
+  ['invalidtype_2',['InvalidType',['../classAppDir_1_1pycommon_1_1pypacker_1_1InvalidType.html',1,'AppDir.pycommon.pypacker.InvalidType'],['../classAppImage_1_1pycommon_1_1pypacker_1_1InvalidType.html',1,'AppImage.pycommon.pypacker.InvalidType'],['../classpycommon_8old_1_1pypacker_1_1InvalidType.html',1,'pycommon.old.pypacker.InvalidType'],['../classpyedpro_1_1pycommon_1_1pypacker_1_1InvalidType.html',1,'pyedpro.pycommon.pypacker.InvalidType']]],
+  ['itemhistory_3',['itemhistory',['../classAppDir_1_1pedlib_1_1pedwin_1_1itemhistory.html',1,'AppDir.pedlib.pedwin.itemhistory'],['../classAppImage_1_1pedlib_1_1pedwin_1_1itemhistory.html',1,'AppImage.pedlib.pedwin.itemhistory'],['../classpyedpro_1_1pedlib_1_1pedwin_1_1itemhistory.html',1,'pyedpro.pedlib.pedwin.itemhistory']]]
 ];
