53eb1e25b1fabbf4975ec3c7ad6e9f2c0eae6ebc
Peter Glen <peterglen@gmail.com>
2023-02-28 23:49:05 -0500

    autocheck

--- pyedpro/pedlib/pedbuffs.py
+++ pyedpro/pedlib/pedbuffs.py
@@ -17,16 +17,14 @@ from gi.repository import GObject
 from pedlib import pedconfig
 from pedlib.pedutil import *
 
-gl_dialog = None
-
-def fill_list(self, self2):
+def fill_list(dialog):
 
     blist = []; was = -1
-    nn2 = self2.notebook.get_current_page()
-    vcurr2 = self2.notebook.get_nth_page(nn2)
-    cc = self2.notebook.get_n_pages()
+    nn2 = dialog.self2.notebook.get_current_page()
+    vcurr2 = dialog.self2.notebook.get_nth_page(nn2)
+    cc = dialog.self2.notebook.get_n_pages()
     for mm in range(cc):
-        vcurr = self2.notebook.get_nth_page(mm)
+        vcurr = dialog.self2.notebook.get_nth_page(mm)
         if was == -1 and vcurr == vcurr2:
             was = mm
         strx = ""
@@ -39,16 +37,23 @@ def fill_list(self, self2):
         strx += vcurr.area.fname
         blist.append(strx)
 
-    update_treestore(gl_dialog, gl_dialog, blist, was)
+    update_treestore(dialog, dialog, blist, was)
 
-def rmbuffer(self, self2, which):
-    cc = self2.notebook.get_n_pages()
+def rmbuffer(dialog, which):
+    cc = dialog.self2.notebook.get_n_pages()
+    #print("which", which)
     for mm in range(cc):
-        vcurr = self2.notebook.get_nth_page(mm)
-        if which == vcurr.area.fname:
-            print("Would remove", vcurr.area)
+        try:
+            vcurr = dialog.self2.notebook.get_nth_page(mm)
+            #print("Iter name", vcurr.area.fname, which)
+            if which == vcurr.area.fname:
+                #print("Would remove", vcurr.area)
+                dialog.self2.mained.close_document(vcurr.area)
+        except:
+            pass
 
 # -------------------------------------------------------------------------
+# Confusion into object oriented implementation -- we just decorate dialog
 
 def buffers(self, self2):
 
@@ -63,9 +68,7 @@ def buffers(self, self2):
     dialog.set_default_response(Gtk.ResponseType.ACCEPT)
     dialog.set_transient_for(self2.mained.mywin)
     dialog.set_position(Gtk.WindowPosition.CENTER)
-    self.dialog = dialog
-    global gl_dialog
-    gl_dialog = dialog
+    dialog.self2 = self2
 
     try:
         dialog.set_icon_from_file(get_img_path("pyedpro_sub.png"))
@@ -80,9 +83,11 @@ def buffers(self, self2):
     dialog.tree = create_tree(self, dialog)
     dialog.tree.set_headers_visible(False)
 
-    dialog.tree.connect("cursor-changed",  tree_sel_row, dialog, self2)
     dialog.tree.connect("key-press-event", area_key, dialog)
     dialog.tree.connect("key-release-event", area_key, dialog)
+    dialog.tree.connect("key-release-event", area_key, dialog)
+    dialog.tree.connect("cursor-changed",  tree_sel_row, dialog, self2)
+    dialog.tree.connect("row-activated",  tree_sel, dialog)
 
     stree = Gtk.ScrolledWindow()
     stree.add(dialog.tree)
@@ -94,9 +99,7 @@ def buffers(self, self2):
     #dialog.vbox.set_spacing(8)
 
     dialog.vbox.pack_start(frame, 1, 1, 0)
-
-    fill_list(self, self2)
-
+    fill_list(dialog)
     dialog.show_all()
     response = dialog.run()
     dialog.destroy()
@@ -119,15 +122,18 @@ def buffers(self, self2):
 
 # ------------------------------------------------------------------------
 
-def del_item(self, self2):
+def del_item(dialog):
 
-    sel = gl_dialog.tree.get_selection()
+    sel = dialog.tree.get_selection()
     xmodel, xiter = sel.get_selected_rows()
     # In muti selection, only process first
     for aa in xiter:
-        xstr = xmodel.get_value(xmodel.get_iter(aa), 0)
-        print ("Selected:", xstr)
-        rmbuffer(self, self2, xstr)
+        ii = xmodel.get_iter(aa)
+        xstr = xmodel.get_value(ii, 0)
+        ystr =  xstr[4:]
+        #print ("Selected for removal:", ystr)
+        rmbuffer(dialog, ystr)
+        xmodel.remove(ii)
         break
 
 def area_key(area, event, dialog):
@@ -154,9 +160,8 @@ def area_key(area, event, dialog):
         if event.keyval == Gdk.KEY_d or \
                 event.keyval == Gdk.KEY_D:
             if area.alt:
-                print("Alt D")
-                del_item(self, self2)
-
+                #print("Alt D")
+                del_item(dialog)
 
     elif  event.type == Gdk.EventType.KEY_RELEASE:
         if event.keyval == Gdk.KEY_Alt_L or \
@@ -164,6 +169,23 @@ def area_key(area, event, dialog):
             area.alt = False;
 
 # ------------------------------------------------------------------------
+
+def tree_sel(xtree, xiter, xpath, dialog):
+
+    #print ("tree_sel", xtree, xiter, xpath)
+    sel = xtree.get_selection()
+    xmodel, xpath = sel.get_selected_rows()
+    if xpath:
+        #print("sel: ", "'" + xpath + "'" )
+        for aa in xpath:
+            xiter2 = xmodel.get_iter(aa)
+            xstr = xmodel.get_value(xiter2, 0)
+            print("mul selstr: ", "'" + xstr + "'" )
+            #if click_dir_action(xstr):
+            #    dialog.xmulti = []
+            #    populate(dialog)
+            #    return
+        dialog.response(Gtk.ResponseType.ACCEPT)
 
 def tree_sel_row(xtree, dialog, self2):
 
@@ -193,6 +215,23 @@ def start_tree(self, win2):
 
     piter = win2.treestore.append(None, ["Searching .."])
     win2.treestore.append(piter, ["None .."])
+
+
+def tree_sel(xtree, xiter, xpath, dialog):
+
+    #print ("tree_sel", xtree, xiter, xpath)
+    sel = xtree.get_selection()
+    xmodel, xpath = sel.get_selected_rows()
+    if xpath:
+        for aa in xpath:
+            xiter2 = xmodel.get_iter(aa)
+            xstr = xmodel.get_value(xiter2, 0)
+            #print("mul selstr: ", "'" + xstr + "'" )
+            #if click_dir_action(xstr):
+            #    dialog.xmulti = []
+            #    populate(dialog)
+            #    return
+        dialog.response(Gtk.ResponseType.ACCEPT)
 
 # -------------------------------------------------------------------------
 def create_tree(self, win2, match = False, text = None):
