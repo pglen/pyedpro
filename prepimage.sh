2630c7acb3128e62ec8e6d272291c14da73cb66a
Peter Glen <peterglen@gmail.com>
2024-03-30 12:49:10 -0400

    autocheck

--- prepimage.sh
+++ prepimage.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-mkdir -p AppImage
-rm -rf AppImage/*
-cp -a pyedpro/* AppImage
-find AppImage -name "*.pyc" -exec rm {} \;
-ARCH=x86_64 appimagetool AppImage
-rm -rf AppImage
-rm -rf AppDir
-rm -rf appimage-build
+TARG=/tmp/AppImage_pyedpro
+
+mkdir -p $TARG
+rm -rf $TARG/*
+cp -a pyedpro/* $TARG
+find $TARG -name "*.pyc" -exec rm {} \;
+ARCH=x86_64 appimagetool $TARG
 mv pyedpro.py-x86_64.AppImage ../largefiles
+rm -rf $TARG
 
 # EOF
