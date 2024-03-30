6cfb1b2a58e64c94e73a24c128073ab36da7881e
Peter Glen <peterglen@gmail.com>
2024-03-30 13:49:54 -0400

    autocheck

--- pyedpro/pyedpro.py
+++ pyedpro/pyedpro.py
@@ -94,7 +94,6 @@ _ = gettext.gettext
 
 import gi; gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
-from gi.repository import Gtk
 from gi.repository import Gdk
 from gi.repository import GLib
 from gi.repository import Gio
