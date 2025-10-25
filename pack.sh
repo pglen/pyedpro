8e07b1d30f476ae54c8866745baee97d1e7c7559
pglen <peterglen99@gmail.com>
2025-10-25 14:10:08 +0700

    autocheck

--- pack.sh
+++ pack.sh
@@ -3,8 +3,21 @@
 # Pack current project. Will back out from dir and create dirname.tgz
 # and put it back to our dir.
 
-CURR=`pwd | awk -F '/' {'print $NF'} |  sort -n | tail -1`
+if [ ! -f pyedpro.png ] ; then
+    echo "Please start from pyedpro directory"
+    exit
+fi
+
+#CURR=`pwd | awk -F '/' {'print $NF'} |  sort -n | tail -1`
+VAR=$(grep "VERSION.*=" pyedpro/pyedpro.py | awk '{print $3}' | sed "s/\"//g")
+CURR=../pyedpro-$VAR.tar.gz
+#echo "name:" $CURR ; exit
+if [ -f $CURR ] ; then
+    echo "File \"$CURR\" exist. Please delete to recreate."
+    exit
+fi
+
 cd ..
-echo "Packing / Archiving project \"$CURR\" "
-tar cfz $CURR.tgz $CURR
-echo Created $CURR.tgz in parent directory
+echo "Packing / Archiving project into \"$CURR\" "
+tar cfz $CURR pyedpro
+#echo Created $CURR in parent directory
